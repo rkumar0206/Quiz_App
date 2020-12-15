e82d4ca6e647afc0470e1d33a5e9935eb0f62eb1
Rohit <rkumar8092378845@gmail.com>
2020-12-15 23:18:40 +0530

    -> added click listeners in HomeFragment.kt

--- app/src/main/java/com/rohitthebest/quizzed_aquizapp/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/rohitthebest/quizzed_aquizapp/ui/fragments/HomeFragment.kt
@@ -3,19 +3,59 @@ package com.rohitthebest.quizzed_aquizapp.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.rohitthebest.quizzed_aquizapp.R
 import com.rohitthebest.quizzed_aquizapp.databinding.FragmentHomeBinding
+import com.rohitthebest.quizzed_aquizapp.databinding.HomeLayoutBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var includeBinding: HomeLayoutBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentHomeBinding.bind(view)
+        includeBinding = binding.include
 
+        initListeners()
+    }
+
+    private fun initListeners() {
+
+        includeBinding.startQuizOption.setOnClickListener(this)
+        includeBinding.chooseCategoryOption.setOnClickListener(this)
+        includeBinding.customizeQuizOption.setOnClickListener(this)
+        includeBinding.savedQuestionsOption.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+
+            includeBinding.startQuizOption.id -> {
+
+                findNavController().navigate(R.id.action_homeFragment_to_quizFragment)
+            }
+
+            includeBinding.chooseCategoryOption.id -> {
+
+                findNavController().navigate(R.id.action_homeFragment_to_chooseCategoryFragment)
+            }
+
+            includeBinding.customizeQuizOption.id -> {
+
+                findNavController().navigate(R.id.action_homeFragment_to_customQuizFragment)
+            }
+
+            includeBinding.savedQuestionsOption.id -> {
+
+                //todo : open saved questions fragment
+            }
+
+        }
 
     }
 
